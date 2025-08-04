e218bdf1510bd78ed0bfe27776dbd453857a0418
abduqodir09 <abduqodiriskandarov2012@gmail.com>
2025-08-04 09:46:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let body = document.querySelector('body')
+
+let tom = function(){
+    body.style.background = 'bisque'
+}
+tom()
+
+let bom = function(){
+    body.style.background = 'black'
+}
+bom()
+
+let one = document.getElementsByClassName('no')
+
+let two = function(){
+    body.style.color = 'white'
+}
+one()
